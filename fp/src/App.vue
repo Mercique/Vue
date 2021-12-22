fdd6a34e755d5b780dc7e643a226f447e0d09088
mercique <mercy1ess@list.ru>
2021-12-22 16:11:00 +0300

    completed task3

--- fp/src/App.vue
+++ fp/src/App.vue
@@ -1,31 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Calculator />
+    <div class="wrapper">
+      <header>
+        <h1 class="title">My personal costs</h1>
+      </header>
+      <main>
+        <add-payment-form @addNewPayment="addToPaymentList"/>
+        <payments-display :items="paymentsList" />
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Calculator from './components/Calculator.vue'
+import AddPaymentForm from "./components/AddPaymentForm.vue";
+import PaymentsDisplay from "./components/PaymentsDisplay.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Calculator
-  }
-}
+  components: { PaymentsDisplay, AddPaymentForm },
+  name: "App",
+  data() {
+    return {
+      show: true,
+      paymentsList: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      return [
+        {
+          date: "28.03.2020",
+          category: "Food",
+          value: 169,
+        },
+        {
+          date: "24.03.2020",
+          category: "Transport",
+          value: 360,
+        },
+        {
+          date: "24.03.2020",
+          category: "Food",
+          value: 532,
+        },
+        {
+          date: "24.03.2020",
+          category: "Internet",
+          value: 532,
+        },
+      ];
+    },
+    addToPaymentList(props) {
+      console.log('run')
+      // this.paymentsList.push(data)
+      this.paymentsList = [...this.paymentsList, props];
+    },
+  },
+  created() {
+    this.paymentsList = this.fetchData();
+  },
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  font-size: 40px;
 }
 </style>
---
+++ fp/src/components/PaymentsDisplay.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+      <div class="box head">
+          <div class="head__box">#</div>
+          <div class="head__box">Date</div>
+          <div class="head__box">Category</div>
+          <div class="head__box">Value</div>
+      </div>
+      <div class="box item" v-for="item, idx in items" :key="idx">
+          <div class="item__box">{{ idx }}</div>
+          <div class="item__box">{{ item.date }}</div>
+          <div class="item__box">{{ item.category }}</div>
+          <div class="item__box">{{ item.value }}</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "PaymentsDisplay",
+    props: {
+        items: {
+            type: Array,
+            default: ()=>[],
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.wrapper {
+    width: 585px;
+}
+.box {
+    display: flex;
+    justify-content: space-between;
+    font-weight: 700;
+    border-bottom: 1px solid #f2f2f2;
+    div {
+        width: 150px;
+        padding: 10px;
+        &:first-child {
+            width: 60px;
+        }
+        &:last-child {
+            width: 50px;
+        }
+    }
+}
+.item {
+    font-weight: 400;
+}
+</style>
