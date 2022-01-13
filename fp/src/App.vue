39633f5aa8cf9551ee583cfebf4dd47f09c4f720
Ilya Chvanov <mercy1ess@list.ru>
2022-01-13 14:33:05 +0300

    switch page #hash

--- fp/src/App.vue
+++ fp/src/App.vue
@@ -2,60 +2,44 @@
   <div id="app">
     <div class="wrapper">
       <header>
+        <a href="#dashboard">dashboard</a> / 
+        <a href="#about">about</a> /
+        <a href="#notfound">notfound</a>
         <h1 class="title">My personal costs</h1>
       </header>
       <main>
-        <add-payment-form />
-        <payments-display :items="currentElements" />
-        <pagination
-          :cur="page"
-          :n="n"
-          :length="paymentsList.length"
-          @paginate="changePage"
-        />
+        <Dashboard v-if="page === 'dashboard'" />
+        <About v-else-if="page === 'about'" />
+        <NotFound v-else />
       </main>
     </div>
   </div>
 </template>
 
 <script>
-import AddPaymentForm from "./components/AddPaymentForm.vue";
-import PaymentsDisplay from "./components/PaymentsDisplay.vue";
-import { mapMutations, mapGetters, mapActions } from "vuex";
-import Pagination from "./components/Pagination.vue";
+import Dashboard from "./views/Dashboard.vue";
+import About from "./views/About.vue";
+import NotFound from "./views/NotFound.vue";
 
 export default {
-  components: { PaymentsDisplay, AddPaymentForm, Pagination },
   name: "App",
+  components: { Dashboard, About, NotFound },
   data() {
     return {
-      show: true,
-      page: 1,
-      n: 3,
+      page: ""
     };
   },
-  computed: {
-    ...mapGetters({ paymentsList: "getPaymentsList" }),
-    total() {
-      return this.$store.getters.getPaymentsListFullValuePrice;
-    },
-    currentElements() {
-      const { n, page } = this;
-      return this.paymentsList.slice(n * (page - 1), n * (page - 1) + n);
-    },
-  },
   methods: {
-    ...mapMutations({ fetch: "setPaymentsListData" }),
-    ...mapActions(["fetchData"]),
-    changePage(p) {
-      this.page = p;
-    },
+    setPage() {
+      this.page = location.hash.slice(1);
+    }
   },
-  created() {
-    this.fetchData();
-    // this.$store.dispatch('fetchData');
-    // this.$store.commit('setPaymentsListData', this.fetchData());
-  },
+  mounted() {
+    this.setPage();
+    window.addEventListener('hashchange', () => {
+      this.setPage();
+    })
+  }
 };
 </script>
 
--- fp/src/store/index.js
+++ fp/src/store/index.js
@@ -43,12 +43,16 @@ export default new Vuex.Store({
             "page1": [
               { "id": 1, "date": "20.03.2020", "category": "Food", "value": 169 },
               { "id": 2, "date": "21.03.2020", "category": "Navigation", "value": 50 },
-              { "id": 3, "date": "22.03.2020", "category": "Sport", "value": 450 }
+              { "id": 3, "date": "22.03.2020", "category": "Sport", "value": 450 },
+              { "id": 4, "date": "23.03.2020", "category": "Sport", "value": 750 },
+              { "id": 5, "date": "24.03.2020", "category": "Sport", "value": 1450 }
             ],
             "page2": [
-              { "id": 4, "date": "23.03.2020", "category": "Entertaiment", "value": 969 },
-              { "id": 5, "date": "24.03.2020", "category": "Education", "value": 1500 },
-              { "id": 6, "date": "25.03.2020", "category": "Food", "value": 200 }
+              { "id": 6, "date": "25.03.2020", "category": "Entertaiment", "value": 969 },
+              { "id": 7, "date": "26.03.2020", "category": "Education", "value": 1500 },
+              { "id": 8, "date": "27.03.2020", "category": "Food", "value": 20 },
+              { "id": 9, "date": "28.03.2020", "category": "Food", "value": 2000 },
+              { "id": 10, "date": "29.03.2020", "category": "Food", "value": 8 }
             ],
           }
           for(let el in obj) {
